51e98fdf80404cdfa660fdf25fd94b8ac73decee
Thomas Winkley <thomas.winkley@gmail.com>
2018-10-08 16:10:34 -0600

    First push of assets
    first push of test assets

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    [SerializeField]GameObject camera;
+    [SerializeField] GameObject followObject;
+    [SerializeField] Transform cameraTransform;
+    Vector3 followTransform;
+
+    // Use this for initialization
+    void Start()
+    {
+        camera = gameObject;
+    }
+	// Update is called once per frame
+	void Update () {
+
+        camera.transform.position = new Vector3(followObject.transform.position.x + 3f, followObject.transform.position.y + 4, followObject.transform.position.z + 3f);
+		
+	}
+}
